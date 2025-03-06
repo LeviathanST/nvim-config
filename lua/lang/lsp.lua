2f162ccbc4a4c8f577e3f1a3b48035784483121e
LeviathanST <phamquochung170505@gmail.com>
2025-03-07 01:46:27 +0700

    update(lang): change lsp capabilities into blink

--- lua/lang/lsp.lua
+++ lua/lang/lsp.lua
@@ -1,13 +1,12 @@
 local M = {}
 
 M.on_attach = function(_, bufnr)
-	local opts = { silent = true, noremap = true }
-	local function buf_set_keymap(mode, mapping, command)
-		vim.api.nvim_buf_set_keymap(bufnr, mode, mapping, command, opts)
-	end
+  local opts = { silent = true, noremap = true }
+  local function buf_set_keymap(mode, mapping, command)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, mapping, command, opts)
+  end
 
-	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
-	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
 end
-M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.capabilities = require("blink.cmp").get_lsp_capabilities()
 return M
