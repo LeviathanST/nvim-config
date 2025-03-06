54c5c5f3ae728a613171b2d9fe79d5b750527605
LeviathanST <phamquochung170505@gmail.com>
2025-03-06 12:40:43 +0700

    update(lsp): replace nvim-cmp into magazine.nvim

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,10 +1,39 @@
+local WIDE_HEIGHT = 40
 return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
+			{ "iguanacucumber/mag-nvim-lsp", name = "cmp-nvim-lsp", opts = {} },
 			"hrsh7th/cmp-path",
-			"hrsh7th/nvim-cmp",
+			{
+				"iguanacucumber/magazine.nvim",
+				name = "nvim-cmp",
+				opts = {
+					window = {
+						completion = {
+							border = { "", "", "", "", "", "", "", "" },
+							winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+							scrollbar_winhighlight = "EndOfBuffer:PmenuSbar,NormalFloat:PmenuSbar",
+							scrollbar_thumb_winhighlight = "EndOfBuffer:PmenuThumb,NormalFloat:PmenuThumb",
+							winblend = vim.o.pumblend,
+							scrolloff = 0,
+							col_offset = 0,
+							side_padding = 1,
+							scrollbar = true,
+						},
+						documentation = {
+							max_height = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / vim.o.lines)),
+							max_width = math.floor((WIDE_HEIGHT * 2) * (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
+							border = { "", "", "", " ", "", "", "", " " },
+							winhighlight = "FloatBorder:NormalFloat",
+							scrollbar_winhighlight = "EndOfBuffer:PmenuSbar,NormalFloat:PmenuSbar",
+							scrollbar_thumb_winhighlight = "EndOfBuffer:PmenuThumb,NormalFloat:PmenuThumb",
+							winblend = vim.o.pumblend,
+							col_offset = 0,
+						},
+					},
+				},
+			},
 			{
 				"folke/lazydev.nvim",
 				ft = "lua",
@@ -37,7 +66,6 @@ return {
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 				},
 			})
-
 			for _, filetype in ipairs(filetypes) do
 				local language = setting_languages[filetype]
 				lspconfig[language.lsp_server].setup(language.lsp)
