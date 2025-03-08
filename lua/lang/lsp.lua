749926212b4fd59793adc64eb1660e4549d9e141
LeviathanST <phamquochung170505@gmail.com>
2025-03-08 15:44:50 +0700

    add(lsp): shortcut for rename

--- lua/lang/lsp.lua
+++ lua/lang/lsp.lua
@@ -7,6 +7,7 @@ M.on_attach = function(_, bufnr)
   end
 
   buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  buf_set_keymap("n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>")
 end
 M.capabilities = require("blink.cmp").get_lsp_capabilities()
 return M
