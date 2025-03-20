3da4cf1802ede040f260defddf793f6e467827b1
LeviathanST <phamquochung170505@gmail.com>
2025-03-21 02:28:57 +0700

    add(plugins): qmk

---
+++ lua/plugins/qmk.lua
@@ -0,0 +1,16 @@
+return {
+  "codethread/qmk.nvim",
+  opts = {
+    name = "LAYOUT",
+    layout = {
+      "_ x x x x x x _ _ _ x x x x x x",
+      "_ x x x x x x _ _ _ x x x x x x",
+      "_ x x x x x x _ _ _ x x x x x x",
+      "_ x x x x x x x _ x x x x x x x",
+      "_ _ x x x x x _ _ _ x x x x x _",
+    },
+    comment_preview = {
+      position = "inside",
+    },
+  },
+}
