8085341e3cb27bb85bbc074c118af77da4f0f961
LeviathanST <phamquochung170505@gmail.com>
2025-03-28 02:23:47 +0700

    update(option): use treesitter

--- lua/options.lua
+++ lua/options.lua
@@ -8,12 +8,13 @@ opt.number = true
 opt.termguicolors = true
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.indentexpr = "nvim_treesitter#indentexpr()"
 
 -- NOTE: Tabs, indent
 opt.expandtab = true -- Use spaces instead of tabs
 opt.shiftwidth = 4 -- Shift 4 spaces when tab
 opt.tabstop = 4 -- 1 tab == 4 spaces
-opt.smartindent = true -- Autoindent new lines
+opt.smartindent = false -- Autoindent new lines
 
 --- NOTE: Local buffer
 opt.swapfile = false
