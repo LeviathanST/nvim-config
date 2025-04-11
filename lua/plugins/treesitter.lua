75c8bfa3a1e432b382097c2061be89c7a7246071
LeviathanST <phamquochung170505@gmail.com>
2025-04-11 15:10:32 +0700

    add(svelte): auto enable highlight with svelte ft

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   opts = {
-    ensure_installed = { "lua", "c", "markdown", "kotlin" },
+    ensure_installed = { "lua", "c", "markdown", "kotlin", "svelte", "css", "html" },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
   },
+  config = function()
+    vim.filetype.add({ extension = { svelte = "svelte" } })
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      pattern = "*.svelte",
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
