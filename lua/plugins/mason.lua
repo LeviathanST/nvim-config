75df125fba01b4dee7868db0cbef529449f4f0a7
LeviathanST <phamquochung170505@gmail.com>
2025-10-06 22:37:02 +0700

    plugs/mason: change name of some lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,10 +6,10 @@ return {
       ensure_installed = {
         "lua_ls", --- lua
         "clangd", --- c/c++
-        "kotlin_language_server", --- Kotlin
+        "kotlin-language-server", --- Kotlin
         "sqls", --- SQL
         "ts_ls", --- TS/JS
-        "svelte", --- Svelte
+        "svelte-language-server", --- Svelte
         -- "zls"  Mason cannot use dev-version for ZLS, so I use zvm instead
       },
     },
