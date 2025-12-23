6387bbad2222bf4ccdd53ce64aa0c28264adf349
LeviathanST <phamquochung170505@gmail.com>
2025-12-23 13:54:25 +0700

    autocmds: add update plugins automatically

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,5 +1,11 @@
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
   callback = function()
-    require("lint").try_lint("zlint")
+    require("lint").try_lint()
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("lazy").update({ show = false })
   end,
 })
